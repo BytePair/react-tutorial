69dcaf0bacee666308de35ed30934cb357d559d5
BytePair <bytepair@gmail.com>
2018-02-04 14:33:52 -0500

    Finish Tutorial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+// components
+import Header from './Components/headerComponent/header';
+import Footer from './Components/footerComponent/footer';
+import HomePage from './Components/Pages/homePage';
+import ProductsPage from './Components/Pages/productsPage';
+
+// includes (css)
+import './Assets/css/default.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Empty Page
-      </div>
+      <Router>
+        <div className='App'>
+        
+          <Header/>
+
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/products' component={ProductsPage} />
+
+          <Footer/>
+
+        </div>
+      </Router>
     );
   }
 }
